afc1cc133bda95a40fa87a6f974655de6e82e426
Emma Brandenburg <emma.brandenburg@gmail.com>
2025-11-19 12:25:30 -0500

    fixed item methods
    added getItemData
    added findStatusCode
    added getItemName
    
    fixed serial number
    added status code & status code logic

--- item.java
+++ item.java
@@ -1,35 +1,91 @@
 import java.util.Random;
 
 public class Item {
+    String itemName;
     int serialNum;
     boolean coldCheck;
     double weight;
-    Random random = new Random();
+    String statusCode;
 
     // Default constructor
     public Item(){
+        itemName = null;
         coldCheck = false;
         weight = 0.0;
         serialNum = 0000;
+        statusCode = null;
     }
 
     // Constructor with inputs
-    public Item(boolean cold, double kilograms){
+    public Item(String name, boolean cold, double kilograms){
+        itemName = name;
         coldCheck = cold;
         weight = kilograms;
 
-        // If Item is set to cold, then first num in serialNum is set to 5
+        // If Item is set to cold, then first serial digit is set to 5
         if( cold == true){
+            serialNum = serialGenerator(true);
+        }
+
+        // If Item is not cold, first serial digit is not 5
+        else{
+            serialNum = serialGenerator(false);
+        }
+
+        statusCode = findStatusCode(serialNum);
+
+
+    }
+
+    // public Item methods
+    public String getItemName(){
+        return itemName;
+    }
+
+    public int getSerialNum(){
+        return serialNum;
+    }
+
+    public double getWeight(){
+        return weight;
+    }
+
+    public String getStatusCode(){
+        return statusCode;
+    }
+
+    public String getItemData(){
+        String[] itemArrayData = new String[4];
+        itemArrayData[0] = itemName;
+        itemArrayData[1] = String.valueOf(serialNum);
+        itemArrayData[2] = weight + "kg";
+        if(coldCheck == true){
+            itemArrayData[3] = "cold";
+        }
+        else{
+            itemArrayData[3] = "room-temp";
+        }
+
+        String ItemData = String.join(",", itemArrayData);
+
+        return ItemData;
+    }
+
+    // private Item method
+    private int serialGenerator(boolean ifCold){
+        Random random = new Random();
+        // If the Item is cold
+        if(ifCold == true){
             int tempID = random.nextInt(900) + 100;
             String stringID = String.valueOf(tempID);
             String combindID = "5" + stringID;
             int finalID = Integer.valueOf(combindID);
-            serialNum = finalID;
+            int serialNum = finalID;
+            return serialNum;
         }
-        
-        // If Item is not cold
+
+        // If the Item is not cold
         else{
-            
             // Finding first digit and making sure its not 5.
             int firstNum = random.nextInt(9) + 1;
             while(firstNum == 5){
@@ -41,21 +97,21 @@ public class Item {
             String stringID = String.valueOf(tempID);
             String combindID = firstNum + stringID;
             int finalID = Integer.valueOf(combindID);
-            serialNum = finalID;
-            
-            serialNum = random.nextInt(9000) + 1000;
+            int serialNum = finalID;
+            return serialNum;
+        }
+    }
+
+    private String findStatusCode(int serial){
+        if(serial % 2 == 0){
+            return "s1";
         }
 
-    }
-
-    // Item methods
-    public int getSerialNum(){
-        return serialNum;
-    }
-
-    public double getWeight(){
-        return weight;
+        else{
+            return "s2";
+        }
     }
 }
+
 
 
