da0e8b2a2abce25c461d99ec040ecc06980c6851
aa36016-ai <aa36016@email.vccs.edu>
2025-11-21 15:29:50 -0800

    Create leftSection.java
    I added a left section class

---
+++ leftSection.java
@@ -0,0 +1,20 @@
+class leftSection
+{
+	int xPos;
+	int yPos;
+	station st[];
+
+	public leftSection()
+	{
+		xPos = 0;
+		yPos = 0;
+		st = new station[4];
+
+		for (int i = 0; i < 4; i++)
+		{
+			st[i].setXPos(xPos);
+			st[i].setYPos(yPos);
+			st[i].setZPos(i+1);
+		}
+	}
+}
