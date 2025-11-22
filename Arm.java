cd06531d5c47dcdf666dee3627fd8b6200d1b7d6
Emma Brandenburg <emma.brandenburg@gmail.com>
2025-11-22 14:41:50 -0500

    Added to vendingMachine and fixed Arm

--- Arm.java
+++ Arm.java
@@ -44,20 +44,20 @@ public class Arm {
     }
 
     public int findItemStation(){
-        // possibly boolean coldSpace = section.coldCount()
-        // possibly boolean weightSpace = section.weightCount()
+        // possibly boolean coldSpace = stationCold.numItemCheck()
+        // possibly boolean weightSpace = stationLight.numItemCheck()
         int i;
         String currItemName = currItem.getItemName();
         boolean isCold = currItem.coldCheck();
         double weight = currItem.getWeight();
         int itemSection = findItemSection();
 
-        // Item belongs in cold section
+        // Item belongs in cold station
         if (isCold == true /*&& coldSpace == true */){
             return 5;
         }
 
-        // Item belongs in light weight section
+        // Item belongs in light weight station
         else if(weight < 50 /*&&  weightSpace == true */){
             return 7;
         }
@@ -65,18 +65,18 @@ public class Arm {
         else if (itemSection == 1){
         /*  
             // Checks if a station is full, and place an item if it's not
-            if (section1.numItemCheck() == false){
-                itemSlot = section1.findItemSlot();
+            if (section1.numItemCheck() == true){
+                itemSlot = station.findItemSlot();
                 return 1;
             }
 
-            else if(section3.numItemCheck() == false){
-                itemSlot = section1.findItemSlot();
+            else if(section3.numItemCheck() == true){
+                itemSlot = station.findItemSlot();
                 return 3;
             }
 
-            else if(section5.numItemCheck() == false){
-                itemSlot = section1.findItemSlot();
+            else if(section5.numItemCheck() == true){
+                itemSlot = station.findItemSlot();
                 return 5;
             }
         */
@@ -86,22 +86,22 @@ public class Arm {
         /*  
             // Checks if a station is full, returns section if its not
             if (section2.numItemCheck() == false){
-                itemSlot = section1.findItemSlot();
+                itemSlot = station.findItemSlot();
                 return 2;
             }
 
             else if(section4.numItemCheck() == false){
-                itemSlot = section1.findItemSlot();
+                itemSlot = station.findItemSlot();
                 return 4;
             }
 
             else if(section6.numItemCheck() == false){
-                itemSlot = section1.findItemSlot();
+                itemSlot = station.findItemSlot();
                 return 6;
             }
 
             else if(section8.numItemCheck() == false){
-                itemSlot = section1.findItemSlot();
+                itemSlot = station.findItemSlot();
                 return 8;
             }
         */
