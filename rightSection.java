82e9555343787127f7ba6943f3b612aaa0720f6f
aa36016-ai <aa36016@email.vccs.edu>
2025-11-21 19:39:03 -0800

    Update rightSection.java
    I updated my right section class

--- rightSection.java
+++ rightSection.java
@@ -6,11 +6,19 @@ class rightSection
 
 	public rightSection()
 	{
-		xPos = 0;
-		yPos = 0;
+		xPos = 2;
+		yPos = 1;
 		st = new station[4];
 		st[0] = new station5();
 		st[2] = new station7();
+		
+		for (int i = 0; i < st.length; i++)
+		{
+			if (i != 0 && i != 2)
+			{
+				st[i] = new station();
+			}
+		}
 		
 		for (int i = 0; i < 4; i++)
 		{
